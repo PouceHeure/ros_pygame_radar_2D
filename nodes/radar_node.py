3882d59f832d258255f7e4b1b60891c71b2e693d
PouceHeure <hugo.pousseur@gmail.com>
2020-08-27 19:23:21 +0200

    create own messages

--- nodes/radar_node.py
+++ nodes/radar_node.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Empty
 
+from ros_pygame_radar_2D.msg import PointPolar, RadarPointCloud
 from ros_pygame_radar_2D.pygame_radar_2D.pygame_radar_2D import radar, point
-
 
 class RadarRos: 
 
     def __init__(self,topic):
         self.i = 0
-        rospy.Subscriber(topic, Empty, self.callback, queue_size=10)
+        rospy.Subscriber(topic, RadarPointCloud, self.callback, queue_size=10)
         self.radar = radar.Radar(300,r_lim=300)
         self.radar.start()
     
     def callback(self,data):
-        self.radar.set_points([point.PointPolar(self.i,self.i)])
+        points = []
+        for point in data.points: 
+            new_point = PointPolar(point.theta,point.r)
+            points.append(new_point)
+        self.radar.set_points(points)
         self.i += 10
     
 if __name__ == '__main__': 
