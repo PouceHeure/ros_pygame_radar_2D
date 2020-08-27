8234d7e202eed9e41b64fc6211b6e46c9c617d7a
PouceHeure <hugo.pousseur@gmail.com>
2020-08-27 19:36:13 +0200

    fix theta/deg point

--- nodes/radar_node.py
+++ nodes/radar_node.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 import rospy
 
+# import msgs
 from ros_pygame_radar_2D.msg import PointPolar, RadarPointCloud
+
+# import radar 
 from ros_pygame_radar_2D.pygame_radar_2D.pygame_radar_2D import radar, point
 
 class RadarRos: 
 
-    def __init__(self,topic):
-        self.i = 0
+    def __init__(self,topic,size,**kwargs):
         rospy.Subscriber(topic, RadarPointCloud, self.callback, queue_size=10)
-        self.radar = radar.Radar(300,r_lim=300)
+        self.radar = radar.Radar(size,**kwargs)
+        
+    def start(self):
         self.radar.start()
     
     def callback(self,data):
         points = []
-        for point in data.points: 
-            new_point = PointPolar(point.theta,point.r)
+        for p in data.points: 
+            new_point = point.PointPolar(p.theta,p.r,theta_in_deg=False)
             points.append(new_point)
         self.radar.set_points(points)
-        self.i += 10
     
 if __name__ == '__main__': 
-    rospy.init_node('listener', anonymous=True)
-    RadarRos("test")
+    rospy.init_node('radar_node', anonymous=True)
+    radar_ros = RadarRos("radar",500,r_lim=4)
+    radar_ros.start()
     rospy.spin()
